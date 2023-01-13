dacf95e6e2e9556e13da2b5dc74538b64c458285
abatmust <abat.must@gmail.com>
2023-01-13 17:15:31 +0100

    finish adding assurance to stage

--- resources/views/stagiaires/search.blade.php
+++ resources/views/stagiaires/search.blade.php
@@ -164,7 +164,12 @@
                                 <td class="text-sm text-cyan-900 font-light px-6 py-2 whitespace-nowrap" x-text="stage.affectation">
 
                                 </td>
-                                <td class="text-sm text-cyan-900 font-light px-6 py-2 whitespace-nowrap" x-text="stage.observation">
+                                <td class="text-sm text-cyan-900 font-light px-6 py-2 whitespace-nowrap">
+                                    <div class="" x-text="stage.observation"></div>
+                                            <svg xmlns="http://www.w3.org/2000/svg"  :fill="stage.assurance == 1 ? 'green':'red'" viewBox="0 0 24 24" stroke-width="1.5" stroke="none" class="w-6 h-6">
+                                            <title>Assurance</title>
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                                            </svg>
 
                                 </td>
                                 <td class="text-sm text-cyan-900 font-light px-6 py-2 whitespace-nowrap">
